d6285d437ed24e23dfaff5d9155119b68726aad1
Swarup <swarupkum45@gmail.com>
2022-12-29 20:31:36 +0530

    backend done

--- client/script.js
+++ client/script.js
@@ -1,18 +1,16 @@
-import { bot } from "./assets/bot.svg";
-import { user } from "./assets/user.svg";
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
 
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
 
 let loadInterval;
 
-
 function loader(element) {
     element.textContent = '';
 
     loadInterval = setInterval(() => {
         element.textContent += '.';
-
         if (element.textContent === '....') {
             element.textContent = '';
         }
@@ -20,29 +18,29 @@ function loader(element) {
 }
 
 function typeText(element, text) {
-    let index = 0
+    let index = 0;
 
     let interval = setInterval(() => {
         if (index < text.length) {
-            element.innerHTML += text.charAt(index)
-            index++
+            element.innerHTML += text.charAt(index);
+            index++;
         } else {
-            clearInterval(interval)
+            clearInterval(interval);
         }
     }, 20);
 }
 
 function generateUniqueId() {
-    const timesTamp = Date.now();
+    const timestamp = Date.now();
     const randomNumber = Math.random();
-
     const hexadecimalString = randomNumber.toString(16);
 
     return `id-${timestamp}-${hexadecimalString}`;
 }
 
 function chatStripe(isAi, value, uniqueId) {
-    return (`
+    return (
+        `
         <div class="wrapper ${isAi && 'ai'}">
             <div class="chat">
                 <div class="profile">
@@ -54,7 +52,8 @@ function chatStripe(isAi, value, uniqueId) {
                 <div class="message" id=${uniqueId}>${value}</div>
             </div>
         </div>
-    `);
+    `
+    )
 }
 
 const handleSubmit = async (e) => {
@@ -62,18 +61,36 @@ const handleSubmit = async (e) => {
 
     const data = new FormData(form);
     chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
-
     form.reset();
-
     const uniqueId = generateUniqueId();
-    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
-
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
     chatContainer.scrollTop = chatContainer.scrollHeight;
-
     const messageDiv = document.getElementById(uniqueId);
     loader(messageDiv);
 
+    const response = await fetch('http://localhost:5000/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt')
+        })
+    });
 
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = " ";
+
+    if (response.ok) {
+        const data = await response.json();
+        const parsedData = data.bot.trim();
+
+        typeText(messageDiv, parsedData);
+    } else {
+        const err = await response.text();
+        messageDiv.innerHTML = "Something went wrong";
+        alert(err);
+    }
 }
 
 form.addEventListener('submit', handleSubmit);
@@ -81,4 +98,4 @@ form.addEventListener('keyup', (e) => {
     if (e.keyCode === 13) {
         handleSubmit(e);
     }
-})
+});
